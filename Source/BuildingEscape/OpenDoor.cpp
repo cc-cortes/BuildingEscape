abc801040da7f34be54d54ecb0c02583982b57a8
Chris Cortes <cc.cortes@gmail.com>
2019-08-02 13:00:17 +0100

    Door Rotation and Log Position

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Free and open for all use
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto DoorRotation = GetOwner()->GetTransform().GetRotation();
+
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 00.0f);
+
+	AActor* Owner = GetOwner();
+	Owner->SetActorRotation(NewRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
